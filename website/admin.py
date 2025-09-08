ee9e4367917e9a724e734d7e256d15f41de361ce
Shubham Chauhan <elskerelvish@outlook.com>
2025-09-09 00:08:15 +0530

    admin

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
 # class ItServices(ad)
+
+
+admin.site.register(models.FAQs)
+admin.site.register(models.ItServices)
+admin.site.register(models.UserQueries)
